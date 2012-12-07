59480651122ed844dfbce0bf899c2c78f31115b4
Erick Rivas <chemical.rivas@gmail.com>
2012-12-07 11:56:02 -0500

    Added async form submission via $.ajax using POST

--- www/index.js
+++ www/index.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
   $("#form").hide(0);
-  $("#form").show(500, function(){
+  $("#form").slideDown(500, function(){
     $("#url").focus();
+  });
+  $("#formdata").submit(function(e){
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/",
+      data: $("#formdata").serialize(),
+      success: function(data){
+        $("#shorturl").html("<a href='" + data + "'>"+ data +"</a>");
+        $("#shorturl").slideDown(500);
+      }
+    });
   });
 });
