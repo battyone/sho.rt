30fe242bce3a7a73021abf2cfd801544f8ac4464
Erick Rivas <chemical.rivas@gmail.com>
2012-12-07 11:52:46 -0500

    Relying more heavily on express.js. GET requests for files fully functional, now working on form submission (POST)

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-// NODE Libraries
-var http = require("http"), fs = require("fs"), url = require("url"), 
-  path = require("path");
+// Response Code (to be prepended to response):
+// 1: OK
+// 2: Invalid
+var url = require("url");
 // Express.js
-var express = require("express")
+var express = require("express");
 var server = express();
+// Redis.io
+var redis = require("redis");
 // Server settings
 var host = 'localhost', port = 8080;
-
+// Set directory
 server.use(express.static(__dirname + "/www"));
+// Handle JSON, multipart, urlencoding
+server.use(express.bodyParser());
+// Handle POST requests sent to /
 server.post("/", function(request, response){
-  console.log(request);
+  if((request.body.action.toLowerCase() === "shorten") &&
+      (request.body.url.length > 25)){
+    var p = url.parse(request.body.url);
+    var s = p['href'];
+  }
 });
 
 server.listen(port, host);
