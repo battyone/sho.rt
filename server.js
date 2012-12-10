74d224438f8146688deca6878a986de521bfa0bd
Erick Rivas <chemical.rivas@gmail.com>
2012-12-10 18:08:03 -0500

    Abstracted POST logic so that code is DRY

--- server.js
+++ server.js
@@ -1,25 +1,57 @@
-// Response Code (to be prepended to response):
 // 1: OK
-// 2: Invalid
-var url = require("url");
-// Express.js
+// 2: Invalid Protocol (for now, only HTTP is allowed)
+// 3: URL too short (set minUrlLength to specify this)
+var url = require("url"), path = require("path");
+// Express.js / Redis.io
 var express = require("express");
 var server = express();
-// Redis.io
 var redis = require("redis");
-// Server settings
-var host = 'localhost', port = 8080;
-// Set directory
-server.use(express.static(__dirname + "/www"));
-// Handle JSON, multipart, urlencoding
-server.use(express.bodyParser());
+// Local settings
+var host = 'localhost', port = 80, next = 16777216, minUrlLength = 25;
+require("./sho.rt");
+var Short = new Base64();
+server.use(express.static(__dirname + "/www")); // Sets directory
+server.use(express.bodyParser()); // Handles JSON, multipart, urlencoding
 // Handle POST requests sent to /
 server.post("/", function(request, response){
-  if((request.body.action.toLowerCase() === "shorten") &&
-      (request.body.url.length > 25)){
-    var p = url.parse(request.body.url);
-    var s = p['href'];
-  }
+  var r = processRequest(request);
+  response.write(JSON.stringify(r));
+  response.end();
 });
 
+// Handles a shorten request. Returns a Serve object
+function processRequest(request) {
+  var r = new Serve();
+  if((request.body.action.toLowerCase() === "shorten") &&
+      (request.body.url.length > minUrlLength)){
+    var p = url.parse(request.body.url);
+    if (p.protocol === undefined || p.protocol == "http:"){
+  //    var db = redis.createClient();
+      console.log(p);
+      var s = p['href'];
+      r.mcd = 1;
+      r.msg = path.join(host, Short.encode(next++));
+      console.log(r.msg);
+  //    db.set(
+      console.log(JSON.stringify(r));
+    } else {
+      //Invalid protocol
+      r.mcd = 2;
+      r.msg = "Invalid protocol";
+    }
+  } else {
+    r.mcd = 3;
+    r.msg = "URL too short";
+  }
+  return r;
+}
+
+Serve = function(){
+  this.mcd; // message code
+  this.msg;
+};
+
+server.use(function(request, response, next) {
+  response.send(404);
+});
 server.listen(port, host);
