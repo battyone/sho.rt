30b80b07ac2d6db3620ea2339669af91ef38738f
Erick Rivas <chemical.rivas@gmail.com>
2012-12-10 18:06:19 -0500

    POST request now uses JSON datatype

--- www/index.js
+++ www/index.js
@@ -5,13 +5,23 @@ $(document).ready(function(){
   });
   $("#formdata").submit(function(e){
     e.preventDefault();
+    $("#shorturl").hide(0);
     $.ajax({
       type: "POST",
       url: "/",
       data: $("#formdata").serialize(),
+      dataType: "json",
       success: function(data){
-        $("#shorturl").html("<a href='" + data + "'>"+ data +"</a>");
-        $("#shorturl").slideDown(500);
+        switch(parseInt(data.mcd)){
+          case 1:
+            $("#shorturl").html("<a href='http://" + data.msg + "'>"+ data.msg +"</a>");
+            $("#shorturl").slideDown(500);
+            break;
+          default:
+            $("#shorturl").html("<p>An error ocurred: " + data.msg + "</p>");
+            $("#shorturl").slideDown(500);
+            break;
+        }
       }
     });
   });
