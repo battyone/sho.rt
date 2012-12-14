e2af696a5c211ac4e6ed417d6adc49eb55353c2f
Erick Rivas <chemical.rivas@gmail.com>
2012-12-14 14:18:38 -0500

    Changed codebase to entirely functional after code review from stanistan

--- src/base64.js
+++ src/base64.js
@@ -1,36 +1,26 @@
-assert = require("assert");
 // Handle Base64 conversion
-Base64 = (function(){
-  var _self = this;
-  var _key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-";
-  // Constructor
-  encoder = function(){
-  };
-  // Given a number, create a hash
-  encoder.prototype.encode = function(number){
-    assert(number>-1, "Input must be positive");
-    assert(Number(number) !== Number.POSITIVE_INFINITY && number !==null &&
-          !isNaN(Number(number)), "Invalid input. Detected NaN, Inf or null");
-    var s = "";
-    var remainder = parseInt(number*64);
-    while (remainder !== 0) {
-      s += _key.charAt(remainder % 64);
-      remainder = Math.floor(remainder / 64);
-    }
-    return s;
-  };
-  // Given a hash (string), return its numeric value
-  encoder.prototype.decode = function(string){
-    assert(typeof string == "string", "Attempting to decode non-string input");
-      var val = 0;
-      var vals = [];
-      for (atom in string.split('')){
-        vals.push(_key.indexOf(string[atom]));
-      }
-      for (ele in vals){
-        val += vals[ele]*Math.pow(64, ele);
-      }
-      return val;
-  };
-  return encoder;
-})();
+var _key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-";
+var assert = require("./assertions");
+
+var encoder = function(r, s) {
+  return !!r ? encoder(Math.floor(r/64), s + _key.charAt(r % 64)) 
+             : (!!s ? s : _key.charAt(r));
+}
+
+exports.encode = function(num) {
+  assert.isNum(num);
+  return encoder(parseInt(num), "");
+}
+
+var indexOf = function(c) {
+  return _key.indexOf(c);
+}
+var exponent = function(acc, ele, i) {
+  return acc + ele * Math.pow(64, i);
+}
+
+exports.decode = function(str) {
+  assert.isStr(str);
+  return str.split('').map(indexOf).reduce(exponent);
+}
+
